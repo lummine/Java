9fd1ce9055f3b03d416ab8570bc8c499e6dc3612
lummine <82365397+lummine@users.noreply.github.com>
2022-07-06 14:43:58 +1000

    added fps counter
    output in console

--- main/GamePanel.java
+++ main/GamePanel.java
@@ -12,6 +12,8 @@ public class GamePanel extends JPanel{
     private MouseInputs mouseInputs;
     private int xDelta = 100;
     private int yDelta = 100;
+    private int frames = 0;
+    private long lastCheck = 0;
 
     public GamePanel() {
 
@@ -26,16 +28,12 @@ public class GamePanel extends JPanel{
     public void changeXDelta(int value) {
         
         this.xDelta += value;
-        
-        repaint();
 
     }
 
     public void changeYDelta(int value) {
         
         this.yDelta += value;
-        
-        repaint();
         
     }
 
@@ -44,8 +42,6 @@ public class GamePanel extends JPanel{
         this.xDelta = x;
         this.yDelta = y;
 
-        repaint();
-
     }
 
     public void paintComponent(Graphics g) {
@@ -53,6 +49,18 @@ public class GamePanel extends JPanel{
         super.paintComponent(g);
 
         g.fillRect(xDelta, yDelta, 200, 50);
+
+        frames++;
+
+        if(System.currentTimeMillis() - lastCheck >= 1000) {
+
+            lastCheck = System.currentTimeMillis();
+            System.out.println("FPS - " + frames);
+            frames = 0;
+
+        }
+
+        repaint();
 
     }
     
